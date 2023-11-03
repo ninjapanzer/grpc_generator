c4b643781200dc566ee6d7a9f6c80bdd8ca55a01
Paul Scarrone <paul@scarrone.co>
2023-11-03 00:55:20 -0400

    can build ruby and python

--- lib/cli/src/main.rs
+++ lib/cli/src/main.rs
@@ -1,5 +1,6 @@
 use std::process::Command;
 use clap::{ Parser, Subcommand };
+use clap::parser::ValueSource::CommandLine;
 // use std::path::PathBuf;
 // mod python
 // mod ruby
@@ -13,18 +14,18 @@ struct Cli {
     /// Generate Proto Classes
     #[command(subcommand)]
     generate: Option<GenerateCommands>,
-    /// Path to .proto files
-    #[arg(short, long, default_value=".")]
-    path: String,
-    /// Path to includes
-    #[arg(short, long, default_value="md")]
-    includes: String,
 }
 
 #[derive(Subcommand)]
 enum GenerateCommands {
     /// does testing things
     Generate {
+        /// Path to .proto files
+        // #[arg(short, long, default_value=".")]
+        path: String,
+        /// Path to includes
+        #[arg(short, long, default_value="./includes")]
+        includes: String,
         /// Generate Ruby Proto Classes
         #[arg(short, long)]
         ruby: bool,
@@ -40,17 +41,49 @@ enum GenerateCommands {
 fn main() {
     let cli = Cli::parse();
     match &cli.generate {
-        Some(GenerateCommands::Generate { ruby, python, javascript }) => {
+        Some(GenerateCommands::Generate { ruby, python, javascript, path, .. }) => {
             let mut ran_any_command = false;
+            let mut args = vec![];
 
             if *ruby {
                 println!("printing ruby lists...");
+                Command::new ("mkdir")
+                    .args(["-p", "../../../../lang/ruby/rbi"])
+                    .output()
+                    .expect("failed to execute process");
+                let output = Command::new ("which")
+                    .arg("grpc_tools_ruby_protoc_plugin")
+                    .output()
+                    .expect("things");
+                args.push("--plugin=protoc-gen-grpc=".to_owned() + String::from_utf8_lossy(&output.stdout).trim_end());
+                args.push("--ruby_out=../../../../lang/ruby".to_string());
+                args.push("--grpc_out=../../../../lang/ruby".to_string());
+                args.push("--rbi_out=../../../../lang/ruby/rbi".to_string());
+                // let output = Command::new("protoc")
+                //     .args([
+                //         ("--plugin=protoc-gen-grpc=".to_owned() + String::from_utf8_lossy(&output.stdout).trim_end()).as_str(),
+                //         "--ruby_out=../../../../lang/ruby",
+                //         "--grpc_out=../../../../lang/ruby",
+                //         "--rbi_out=../../../../lang/ruby/rbi",
+                //         "--proto_path=../../../../",
+                //         path
+                //     ]).output()
+                //     .expect("failed to execute process");
+                println!("Standard Output: {}", String::from_utf8_lossy(&output.stdout));
+                println!("Standard Error: {}", String::from_utf8_lossy(&output.stderr));
+                // Command::new("./rscript.sh").status().expect("Failed to run the Ruby script");
                 // Command::new("./rscript.sh").status().expect("Failed to run the Ruby script");
                 ran_any_command = true;
             }
 
             if *python {
+                Command::new ("mkdir")
+                    .args(["-p", "../../../../lang/python/pyi"])
+                    .output()
+                    .expect("failed to execute process");
                 println!("printing python lists...");
+                args.push("--python_out=../../../../lang/python".to_string());
+                args.push("--pyi_out=../../../../lang/python/pyi".to_string());
                 // Command::new("./pscript.sh").status().expect("Failed to run the Python script");
                 ran_any_command = true;
             }
@@ -63,6 +96,16 @@ fn main() {
 
             if !ran_any_command {
                 println!("Not printing testing lists...");
+            } else {
+                let output = Command::new("protoc")
+                    .args(args)
+                    .args([
+                        "--proto_path=../../../../",
+                        path
+                    ]).output()
+                    .expect("failed to execute process");
+                println!("Standard Output: {}", String::from_utf8_lossy(&output.stdout));
+                println!("Standard Error: {}", String::from_utf8_lossy(&output.stderr));
             }
         }
         _ => {}
