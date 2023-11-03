c4ed30ec8825b917ea1f32896e546d0333e196cd
Paul Scarrone <paul@scarrone.co>
2023-11-03 11:18:18 -0400

    setup for python pyi and pydantic

--- flake.nix
+++ flake.nix
@@ -13,10 +13,25 @@
     {
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
-          packages = with pkgs; [ protobuf ruby_3_2 go ];
+          packages = with pkgs; [
+            protobuf
+            ruby_3_2
+            go
+            python310
+            python310Packages.protobuf
+            python310Packages.grpcio-tools
+            python310Packages.mypy-protobuf
+            python310Packages.toml
+          ];
           shellHook = ''
             # Custom environment manipulations can go here
             # Example: export PATH=$PATH:/path/to/custom/bin
+            go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.3.0
+            python -m venv .venv
+            source .venv/bin/activate
+            python -m ensurepip --upgrade
+            pip install --upgrade pip
+            pip install protobuf_to_pydantic
             gem install grpc-tools --bindir=./bin
             export PATH=$(pwd)/bin:$PATH
           '';
